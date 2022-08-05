3582b62b189034515f17ff47f336e388caf6930a
Josef Leyva <jalm_x@hotmail.com>
2022-08-05 16:35:11 -0500

    git:init

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Pyproject
+
+Simply CLI for python project generator
+
+Generate next structure
+
+```
+├── .gitignore
+├── LICENSE.md
+└── src
+    ├── __init__.py
+    └── pyproject.py
+```
+
+## Dependencies
+
---
+++ src/pyproject.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+#enconding=utf-8
+
+
+#create a structure of project basic of python
+
+#.
+#├── .git
+#├── LICENSE.md
+#└── src
+#    ├── __init__.py
+#    └── pyproject.py
+
+import os
+
+DIRECTORY = "src"
+PATH_DIRECTORY = os.path.join("./", DIRECTORY)
+
+def exec_cmd():
+    cmds = [
+            "ignore python",
+            'license-generator install mit -e md -y 2022 -n "Xizuth"'
+            ]
+
+    
+    for cmd in cmds:
+        os.system(cmd)
+
+def create_files(name="main.py"):
+    files = [name,"__init__.py","main.py"]
+    for file in files:
+        open(os.path.join(PATH_DIRECTORY,file), "w")
+
+
+def create_src_folder():
+    try:
+        os.mkdir(PATH_DIRECTORY) 
+        return True
+    except: 
+        return False
+
+def main():
+    create_src_folder()
+    create_files()
+    exec_cmd()
+
+
+if __name__ == "__main__":
+    main()
